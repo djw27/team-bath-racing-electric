9ac18638f1283576ea013103a26a0621db858d65
sheligalo <sheligalo@live.com>
2014-10-13 17:44:49 +0100

    Initial commit
    TBRe website

---
+++ assets/mail/contact_me.php
@@ -0,0 +1,20 @@
+<?php
+// Check for empty fields
+if(empty($_POST['email']) 		||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$email_address = $_POST['email'];
+
+// Create the email and send the message
+$to = 'sheligalo@live.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "New TBRe interest";
+$email_body = "You have received a new sign up to the release of the website.\n\n"."Here is the email:\n\nEmail: $email_address";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
