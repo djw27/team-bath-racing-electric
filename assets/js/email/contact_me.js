111afa1422027f8f5e710e6ccc4d30ee7d7042f3
sheligalo <sheligalo@live.com>
2014-10-13 18:11:48 +0100

    Mail update
    Updated the e-mails used and the display messages showed

--- assets/js/email/contact_me.js
+++ assets/js/email/contact_me.js
@@ -22,7 +22,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thanks. We'll be in touch shortly. </strong>");
+                        .append("<strong>Thanks. We'll be in touch shortly.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -10,10 +10,10 @@ if(empty($_POST['email']) 		||
 $email_address = $_POST['email'];
 
 // Create the email and send the message
-$to = 'sheligalo@live.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@teambathracingelectric.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "New TBRe interest";
 $email_body = "You have received a new sign up to the release of the website.\n\n"."Here is the email:\n\nEmail: $email_address";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@teambathracingelectric.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
