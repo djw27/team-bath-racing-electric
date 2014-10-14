2852f8c78aa1ed674a02e515a612bfba1e0d0cf7
sheligalo <sheligalo@live.com>
2014-10-14 09:10:55 +0100

    Update mobile
    Updated responsiveness of the site

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -15,7 +15,7 @@ COMMING SOON PAGE
     var hours = $('#hours');
     var minutes = $('#minutes');
     var seconds = $('#seconds');
-    
+
     setDate();
     function setDate(){
         var now = new Date();
@@ -47,41 +47,7 @@ COMMING SOON PAGE
 /******************************************************************************************************************************
 ANIMATIONS
 *******************************************************************************************************************************/
-(function($) {
-    "use strict";
-    var isMobile = false;
-    if (navigator.userAgent.match(/Android/i) || 
-        navigator.userAgent.match(/webOS/i) ||
-        navigator.userAgent.match(/iPhone/i) || 
-        navigator.userAgent.match(/iPad/i)|| 
-        navigator.userAgent.match(/iPod/i) || 
-        navigator.userAgent.match(/BlackBerry/i)) {                 
-        isMobile = true;            
-    }
-    if (isMobile == true) {
-        $('body').removeClass('nomobile');
-        $('.animated').removeClass('animated');
-    }
-    $(function() {
-        if (isMobile == false) {
-            $('*[data-animated]').addClass('animated');
-        }
-        function animated_contents() {
-            $(".animated:appeared").each(function (i) {
-                var $this    = $(this),
-                    animated = $(this).data('animated');
 
-                setTimeout(function () {
-                    $this.addClass(animated);
-                }, 50 * i);
-            });
-        }
-        animated_contents();
-        $(window).scroll(function () {
-            animated_contents();
-        });
-    });
-})(jQuery);
 /******************************************************************************************************************************
 SLIDER
 *******************************************************************************************************************************/
@@ -126,6 +92,6 @@ PROGRESS BAR
 (function($) {
     "use strict";
     $("a.btn-progress").click(function(){
-        $('#bar-container').slideToggle(); 
+        $('#bar-container').slideToggle();
     });
 })(jQuery);
